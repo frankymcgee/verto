6775174d0d527f4f4953aeb8d754876dc73cbb05
frankymcgee <46082212+frankymcgee@users.noreply.github.com>
2024-02-10 17:22:56 +0800

    Removed Field Interaction DocType

--- whsms_management/work_health_and_safety_management_system/doctype/field_interaction/field_interaction.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2024, Webwire and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("Field Interaction", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- whsms_management/work_health_and_safety_management_system/doctype/field_interaction/test_field_interaction.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Webwire and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestFieldInteraction(FrappeTestCase):
-	pass
