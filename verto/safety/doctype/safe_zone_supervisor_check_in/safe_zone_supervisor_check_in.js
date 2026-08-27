d9ec7f34a5569b358240ee326d6e153624a2b216
frankymcgee <sean@webwire.com.au>
2026-08-27 12:18:16 +0800

    Added Safe Zone Supervisor Check-In Document

---
+++ verto/safety/doctype/safe_zone_supervisor_check_in/safe_zone_supervisor_check_in.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Webwire and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Safe Zone Supervisor Check-in", {
+// 	refresh(frm) {
+
+// 	},
+// });
