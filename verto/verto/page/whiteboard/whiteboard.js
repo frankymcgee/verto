b02bdfea861171218d32e98d06aab909e160cdd0
frankymcgee <sean@webwire.com.au>
2026-08-31 12:15:20 +0800

    Whiteboard update

--- verto/verto/page/whiteboard/whiteboard.js
+++ verto/verto/page/whiteboard/whiteboard.js
@@ -19,24 +19,64 @@ frappe.pages["whiteboard"].on_page_hide = function (wrapper) {
   }
 };
 
-function load_whiteboard(wrapper) {
-  const parent = wrapper;
+function load_stylesheet(href, id) {
+  const existing = document.getElementById(id);
 
+  if (existing) {
+    return Promise.resolve();
+  }
+
+  return new Promise((resolve, reject) => {
+    const stylesheet = document.createElement("link");
+
+    stylesheet.id = id;
+    stylesheet.rel = "stylesheet";
+    stylesheet.type = "text/css";
+    stylesheet.href = href;
+
+    stylesheet.onload = resolve;
+    stylesheet.onerror = () => {
+      stylesheet.remove();
+      reject(new Error(`Could not load stylesheet: ${href}`));
+    };
+
+    document.head.appendChild(stylesheet);
+  });
+}
+
+async function load_whiteboard(wrapper) {
   if (wrapper.whiteboard_instance) {
     wrapper.whiteboard_instance.destroy();
     wrapper.whiteboard_instance = null;
   }
 
+  const parent = wrapper;
   $(parent).empty();
 
-  frappe.require([
-	"excalidraw.bundle.css",
-	"whiteboard.bundle.css",
-	"whiteboard.bundle.jsx",
-	]).then(() => {
-	wrapper.whiteboard_instance = new frappe.ui.Whiteboard({
-		wrapper: parent,
-		page: wrapper.whiteboard_page,
-	});
-	});
+  try {
+    await load_stylesheet(
+      "/assets/verto/css/excalidraw.css",
+      "verto-excalidraw-css"
+    );
+
+    await frappe.require([
+      "whiteboard.bundle.css",
+      "whiteboard.bundle.jsx",
+    ]);
+
+    wrapper.whiteboard_instance = new frappe.ui.Whiteboard({
+      wrapper: parent,
+      page: wrapper.whiteboard_page,
+    });
+  } catch (error) {
+    console.error("[Verto Whiteboard] Failed to load:", error);
+
+    frappe.msgprint({
+      title: __("Whiteboard Error"),
+      message: __(
+        "The whiteboard assets could not be loaded. Please refresh the page."
+      ),
+      indicator: "red",
+    });
+  }
 }
